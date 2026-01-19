c7bae8987930dc6e1d9859790f745e8a43a49ba3
Andrey Chernikovich <a.chernikovich@softteco.com>
2026-01-20 00:26:52 +0300

    add incoming call popup

---
+++ lib/feature/call/view/widgets/incoming_call_banner.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:lets_talk/common/widget/c_avatar.dart';
+
+class IncomingCallBanner extends StatelessWidget {
+  final String callerName;
+  final String callType;
+  final VoidCallback onAccept;
+  final VoidCallback onDecline;
+
+  const IncomingCallBanner({
+    super.key,
+    required this.callerName,
+    required this.callType,
+    required this.onAccept,
+    required this.onDecline,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          width: double.infinity,
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: const Color(0xFF222222),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.3),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: Row(
+              children: [
+                const CAvatar(
+                  radius: 24,
+                  name: 'Неизвестный Пользователь',
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Incoming ${callType} Call...',
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        callerName,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 12),
+                // Actions
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _CallActionButton(
+                      icon: Icons.close,
+                      color: Colors.red,
+                      onPressed: onDecline,
+                    ),
+                    const SizedBox(width: 16),
+                    _CallActionButton(
+                      icon: Icons.check,
+                      color: Colors.blue,
+                      onPressed: onAccept,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CallActionButton extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final VoidCallback onPressed;
+
+  const _CallActionButton({
+    required this.icon,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          icon,
+          color: Colors.white,
+          size: 28,
+        ),
+      ),
+    );
+  }
+}
--- lib/feature/shell/view/shell_holder.dart
+++ lib/feature/shell/view/shell_holder.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:lets_talk/common/service/websocket_service.dart';
 import 'package:lets_talk/feature/auth/domain/auth_bloc/auth_bloc.dart';
 import 'package:lets_talk/feature/call/domain/bloc/call_bloc.dart';
+import 'package:lets_talk/feature/call/view/widgets/incoming_call_banner.dart';
 import 'package:lets_talk/feature/settings/domain/profile_bloc/profile_bloc.dart';
 import 'package:lets_talk/app/router/routes.dart';
 
@@ -33,37 +34,46 @@ class ShellHolder extends StatelessWidget {
           BlocListener<CallBloc, CallState>(
             listener: (context, state) {
               if (state is CallIncoming) {
-                showDialog(
+                showGeneralDialog(
                   context: context,
                   barrierDismissible: false,
-                  builder: (_) => AlertDialog(
-                    title: const Text('Incoming Call'),
-                    content: Text('User ${state.callerId} is calling you.'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          context
-                              .read<CallBloc>()
-                              .add(CallRejected(callId: state.callId));
-                          context.pop();
-                        },
-                        child: const Text('Decline'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          context.pop();
-                          context.read<CallBloc>().add(CallAccepted(
-                            callId: state.callId,
-                            callerId: state.callerId,
-                            offer: state.offer,
-                            isVideo: state.callType == 'video',
-                          ));
-                          context.push(Routes.calls.path);
-                        },
-                        child: const Text('Accept'),
-                      ),
-                    ],
-                  ),
+                  barrierLabel: 'Incoming Call',
+                  barrierColor: Colors.black54,
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return IncomingCallBanner(
+                      callerName: 'User ${state.callerId}',
+                      callType: state.callType,
+                      onDecline: () {
+                        context
+                            .read<CallBloc>()
+                            .add(CallRejected(callId: state.callId));
+                        context.pop();
+                      },
+                      onAccept: () {
+                        context.pop();
+                        context.read<CallBloc>().add(CallAccepted(
+                          callId: state.callId,
+                          callerId: state.callerId,
+                          offer: state.offer,
+                          isVideo: state.callType == 'video',
+                        ));
+                        context.push(Routes.calls.path);
+                      },
+                    );
+                  },
+                  transitionBuilder: (context, animation, secondaryAnimation, child) {
+                    return SlideTransition(
+                      position: Tween<Offset>(
+                        begin: const Offset(0, -1),
+                        end: Offset.zero,
+                      ).animate(CurvedAnimation(
+                        parent: animation,
+                        curve: Curves.easeOut,
+                      )),
+                      child: child,
+                    );
+                  },
+                  transitionDuration: const Duration(milliseconds: 300),
                 );
               } else if (state is CallOutgoing) {
                 final currentPath = GoRouterState.of(context).uri.path;
@@ -71,7 +81,9 @@ class ShellHolder extends StatelessWidget {
                   context.push(Routes.calls.path);
                 }
               } else if (state is CallEnded) {
-                context.pop();
+                if (context.canPop()) {
+                   context.pop();
+                }
               }
             },
           ),

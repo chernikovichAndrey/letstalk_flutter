204ddd90594762fbde210d76af7e9dce88fda21d
Andrey Chernikovich <a.chernikovich@softteco.com>
2026-01-25 23:24:46 +0300

    refactoring

--- lib/common/extension/routes_ext.dart
+++ lib/common/extension/routes_ext.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lets_talk/app/router/routes.dart';
-import 'package:lets_talk/feature/calls_history/view/call_details_sheet.dart';
+import 'package:lets_talk/feature/calls_history/view/sheet/call_details_sheet.dart';
 import 'package:lets_talk/feature/calls_history/view/sheet/call_contacts_sheet.dart';
 import 'package:lets_talk/feature/chats/view/chat_details_page_scope.dart';
 import 'package:lets_talk/feature/chats/view/sheet/chat_contact_sheet.dart';

73e806f1f50bba5c6e13a8d8e66913509892248a
Andrey Chernikovich <a.chernikovich@softteco.com>
2026-02-11 10:18:09 +0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -10,10 +11,14 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 // }
 
 import 'package:lets_talk/di/injection.dart';
+import 'package:lets_talk/app/config/firebase_options.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await configureDependencies();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,

9f04a8a67cedb15d7710bc0ca78b4e3b1aceaadf
Andrey Chernikovich <a.chernikovich@softteco.com>
2026-02-25 01:09:44 +0300

    refactoring ws reconnect timer

--- lib/common/service/websocket_service.dart
+++ lib/common/service/websocket_service.dart
@@ -8,6 +8,7 @@ import 'package:injectable/injectable.dart';
 import 'package:lets_talk/app/environment/environment.dart';
 import 'package:lets_talk/di/injection.dart';
 import 'package:lets_talk/feature/auth/domain/auth_bloc/auth_bloc.dart';
+import 'package:lets_talk/feature/auth/domain/repository/auth_repository.dart';
 import 'package:lets_talk/feature/shell/connectivity/domain/bloc/connectivity_bloc.dart';
 import 'package:logger/logger.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
@@ -88,17 +89,10 @@ class WebSocketService with WidgetsBindingObserver {
     if (_channel != null) return;
 
     try {
-      final bloc = getIt<AuthBloc>();
-      final token = bloc.state is AuthAuthenticated
-          ? (bloc.state as AuthAuthenticated).token
-          : null;
-
-      final baseUri = Uri.parse(Env.wsUrl);
-      final uri = token != null
-          ? baseUri.replace(queryParameters: {'token': token})
-          : baseUri;
-
-      _channel = WebSocketChannel.connect(uri, protocols: protocols);
+      _channel = WebSocketChannel.connect(
+        Uri.parse(Env.wsUrl),
+        protocols: protocols,
+      );
 
       _socketSubscription = _channel!.stream.listen(
             (data) {
@@ -117,7 +111,11 @@ class WebSocketService with WidgetsBindingObserver {
         },
       );
 
-      _logger.i('WebSocket connected to $uri');
+      _logger.i('WebSocket connected to ${Env.wsUrl}');
+      final bloc = getIt<AuthBloc>();
+      if (bloc.state is AuthAuthenticated) {
+        authenticate((bloc.state as AuthAuthenticated).token!);
+      }
     } catch (e) {
       _logger.e('WebSocket connection error: $e');
       _scheduleReconnect();
@@ -128,8 +126,8 @@ class WebSocketService with WidgetsBindingObserver {
     if (_intentionalDisconnect) return;
     if (_reconnectTimer?.isActive ?? false) return;
 
-    _logger.i('Scheduling WebSocket reconnect in 5 seconds...');
-    _reconnectTimer = Timer(const Duration(seconds: 5), () {
+    _logger.i('Scheduling WebSocket reconnect in 1 seconds...');
+    _reconnectTimer = Timer(const Duration(seconds: 1), () {
       _logger.i('Executing scheduled reconnect...');
       connect();
     });

3e1dfc5b7b1288ed902866fc46cde2773d0a8690
Charnikovich Andrey <andrei.charnikovich@idt.net>
2026-05-14 12:44:32 +0300

    splash screen

--- lib/feature/splash/view/splash_page.dart
+++ lib/feature/splash/view/splash_page.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:lets_talk/common/widget/gradient_background.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GradientBackground(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
+    return const Scaffold(
+      backgroundColor: Color(0xFFC96A3A),
+      body: Center(
+        child: _SplashLogo(),
       ),
+    );
+  }
+}
+
+class _SplashLogo extends StatelessWidget {
+  const _SplashLogo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/splash_icon.png',
     );
   }
 }
